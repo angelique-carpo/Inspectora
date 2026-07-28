984064492cb0217b53a9e3f035c98c1668db8585
Seedswirl <angelique.carpo@gmail.com>
2026-07-28 12:39:21 +0300

    refactor: redesign crawler architecture with single-fetch analysis flow

---
+++ src/main/java/io/github/angeliquecarpo/inspectora/crawl/SitemapReader.java
@@ -0,0 +1,11 @@
+package io.github.angeliquecarpo.inspectora.crawl;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SitemapReader {
+
+    public Set<String> getUrls(String website) {
+        return new HashSet<>();
+    }
+}
---
+++ src/main/java/io/github/angeliquecarpo/inspectora/crawl/UrlProvider.java
@@ -0,0 +1,12 @@
+package io.github.angeliquecarpo.inspectora.crawl;
+
+import java.util.Set;
+
+public class UrlProvider {
+    public Set<String> getUrls(String website){
+        return null;
+    }
+    private boolean hasSitemap(String website){
+        return false;
+    }
+}
